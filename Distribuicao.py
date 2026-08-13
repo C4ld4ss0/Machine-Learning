d49b248e97925179af74694e1994d5d880036850
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-13 21:59:51 +0000

    Finaliza exercicio de estatistica descritiva

--- Distribuicao.py
+++ Distribuicao.py
@@ -1,2 +1,21 @@
+
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis
+
+df = pd.read_csv("census.csv", sep=",")
+idades = df['age']
+
+idades = pd.DataFrame({
+
+
+})
+
+
+
+obliquidade = skew(idades)
+
+curtose = kurtosis(idades)
+
+print("Obliquidade é: ", obliquidade)
+print("Curtose é: ", curtose)

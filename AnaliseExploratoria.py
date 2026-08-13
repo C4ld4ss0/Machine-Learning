4cf0e38dacf970036643bcb875fcc523c670f5bc
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-13 05:27:22 +0000

    Finaliza exercicio de estatistica descritiva

--- AnaliseExploratoria.py
+++ AnaliseExploratoria.py
@@ -1,5 +1,23 @@
 import pandas as pd
+import numpy as np
+import statistics
 
-df = pd.read_csv("censo.csv", sep=";")
+df = pd.read_csv("census.csv", sep=",")
+idades = df['age']
+mediaAritmetica = idades.mean()
+mediaHarmonica = statistics.harmonic_mean(idades)
+mediaGeometrica = statistics.geometric_mean(idades)
+mediaQuadratica = np.sqrt((idades**2).mean())
+mediana = idades.median()
+moda = idades.mode()[0]
 
-print(df.head())
+print ("--Resultados--")
+print (f"A Média aritmética é: {mediaAritmetica:.2f}")
+print (f"A Média Harmonica é: {mediaHarmonica:.2f}")
+print (f"A Média Geométrica é: {mediaGeometrica:.2f}")
+print (f"A média Quadrática é: {mediaQuadratica:.2f}")
+print ("A Mediana é: ", mediana)
+print ("A Moda é: ", moda)
+
+# print(df.head())
+# print(idades.head())

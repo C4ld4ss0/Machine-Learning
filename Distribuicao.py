fe7b9ac7db0c5e5a8ab8737d1cbddb1ce3ff129f
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-14 10:44:11 -0300

    Update Distribuicao.py

--- Distribuicao.py
+++ Distribuicao.py
@@ -5,14 +5,28 @@ from scipy.stats import skew, kurtosis
 
 df = pd.read_csv("census.csv", sep=",")
 idades = df['age']
-
+# Histograma
 tabela_Idades = pd.DataFrame({'idade': idades})
+plt.figure(figsize=(10,7))
+plt.hist(idades, bins = 10, edgecolor = 'black')
+plt.title('Distribuição de idade')
+plt.xlabel('idade')
+plt.ylabel('Frequência')
+plt.tight_layout()
+plt.show()
 
+# Box plot
+plt.boxplot(idades)
+plt.title("Bloxplot das idades")
+plt.ylabel("Idade em anos")
 
 
 obliquidade = skew(idades)
+
 
 curtose = kurtosis(idades)
 
 print("Obliquidade é: ", obliquidade)
 print("Curtose é: ", curtose)
+plt.clf()
+plt.show()

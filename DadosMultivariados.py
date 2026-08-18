1ea3956487ce351eb8b0caa441578540bccd4689
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-18 18:53:40 -0300

    Update DadosMultivariados.py
    Inserção dos dados

--- DadosMultivariados.py
+++ DadosMultivariados.py
@@ -3,4 +3,15 @@ import matplotlib.pyplot as plt
 import nunpy as np
 import scipy.stats import mode
 
+# Dados informados pela própria professora
+dados = {
+ 'Horas_Estudo': [2, 3, 4, 5, 6, 7, 8, 9, 10, 6],
+ 'Frequencia': [65, 70, 72, 78, 80, 85, 88, 92, 95, 82],
+ 'Nota_Prova': [5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 7.2],
+ 'Nota_Trabalho': [5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.2, 7.8]
+}
+
+df = pd.DataFrame(dados)
+print(dados.mean())
+
 

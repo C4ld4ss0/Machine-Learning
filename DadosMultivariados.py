971e75b0e293c0125553f811b95dfde8ee0ea040
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-26 01:19:25 +0000

    Atividade Realizada

--- DadosMultivariados.py
+++ DadosMultivariados.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import nunpy as np
-import scipy.stats import mode
+import numpy as np
+from scipy.stats import mode
 
 # Dados informados pela própria professora
 dados = {
@@ -12,6 +12,43 @@ dados = {
 }
 
 df = pd.DataFrame(dados)
-print(dados.mean())
 
+print("--- Médias ---")
+print(df.mean())
 
+print("\n--- Medianas ---")
+print(df.median())
+
+print("\n--- Modas ---")
+print(df.mode())
+
+print("\n--- Amplitude ---")
+amplitude = df.max() - df.min()
+print(amplitude)
+
+print("\n--- Variância ---")
+print(df.var().round(2))
+
+print("\n--- Desvio Padrão ---")
+print(df.std().round(2))
+
+print("\n--- Matriz de Covariância ---")
+print(df.cov().round(2))
+
+print("\n --- Matriz de Correlação ---")
+print(df.corr().round(2))
+
+plt.figure(figsize=(8,6))
+plt.scatter(df['Horas_Estudo'], df['Nota_Prova'], color='#34AAF4', edgecolor='black')
+plt.title('Relação entre horas e Nota da Prova')
+plt.xlabel('Horas de Estudo')
+plt.ylabel('Nota da Prova')
+plt.grid(axis='both', linestyle='--', alpha=0.5)
+
+plt.savefig("Dispersao.png")
+plt.clf()
+
+tabela_final = df.agg(['mean', 'median', 'var', 'std', 'min', 'max']).T
+
+print('\n--- Tabela Resumo ---')
+print(tabela_final.round(2))

ea6e5e4e53ff141d514831b56d0f01d8354b88a1
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-09-01 18:47:58 -0300

    Create Knn.py

---
+++ Knn.py
@@ -0,0 +1,9 @@
+import panda as pd
+import nunpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklears.model_selections import train_test_split
+from sklearn.preprocessing import StandartScaler
+from sklearn.neighbors import KNeighborsClassfier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, ConfusionMatrixDisplay
+
